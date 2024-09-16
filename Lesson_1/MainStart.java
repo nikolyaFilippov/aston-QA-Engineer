7f0886242113dfcb524f13a07a6872670dbf74a3
nikolyaFilippov <nikolya_filippov_88@mail.ru>
2024-09-16 19:10:32 +0300

    lesson-1

--- Lesson_1/MainStart.java
+++ Lesson_1/MainStart.java
@@ -3,15 +3,15 @@ package Lesson_1;
 public class MainStart {
     public static void main(String[] args) {
         Function fan = new Function();
-        fan.printThreeWords();
-        fan.checkSumSign();
-        fan.printColor();
-        fan.compareNumbers();
-        fan.comparisonNumber(5,21);
-        fan.printWor(10);
-        fan.printWorBoolean(-10);
-        fan.printString("Привет",5);
-        fan.isLeapYear(400);
+        fan.printThreeWords();//1 задание
+        fan.checkSumSign();//2 задание
+        fan.printColor();//3 задание
+        fan.compareNumbers();//4 задание
+        fan.comparisonNumber(5,21);//5 задание
+        fan.printWor(10);//6 задание
+        fan.printWorBoolean(-10);//7 задание
+        fan.printString("Привет",5);//8 задание
+        fan.isLeapYear(400);//9 задание
 
 
 
